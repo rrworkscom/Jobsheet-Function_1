8cad314bb0c93ae69d171eef08124ba3b6331261
rrworkscom <rrworks010@gmail.com>
2025-12-01 11:35:48 +0700

    Addition Greetings added

--- Gratitude20.java
+++ Gratitude20.java
@@ -1,10 +1,19 @@
 public class Gratitude20 {
-    public static void sayThankyou() {
-        System.out.println("Thank you for being the best teacher in the world.\n " 
-        + "You inspired me a love for learning and made me feel like I could ask you  anything.");
+
+    public static void sayThankYou() {
+        System.out.println(
+            "Thank you for being the best teacher in the world.\n" +
+            "You inspired in me a love for learning and made me feel like I could ask you anything."
+        );
+    }
+
+    public static void sayAdditionalGreetings(String greeting) {
+        System.out.println(greeting);
     }
 
     public static void main(String[] args) {
-        sayThankyou();
+        sayThankYou();
+        String expression = "Thank you... wish you all the best!!";
+        sayAdditionalGreetings(expression);
     }
 }
