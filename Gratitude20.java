78ef45f1662e8858e2fd6ec95f2867451ea7ad22
rrworkscom <rrworks010@gmail.com>
2025-12-01 10:25:38 +0700

    Gratitude20.java

---
+++ Gratitude20.java
@@ -0,0 +1,10 @@
+public class Gratitude20 {
+    public static void sayThankyou() {
+        System.out.println("Thank you for being the best teacher in the world.\n " 
+        + "You inspired me a love for learning and made me feel like I could ask you  anything.");
+    }
+
+    public static void main(String[] args) {
+        sayThankyou();
+    }
+}
