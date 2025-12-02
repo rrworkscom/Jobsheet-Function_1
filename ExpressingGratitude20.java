9e8d33439d0877c46cade5bfad44acd3f0d4a2cc
rrworkscom <rrworks010@gmail.com>
2025-12-02 18:19:16 +0700

    Remark added

--- ExpressingGratitude20.java
+++ ExpressingGratitude20.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class ExpressingGratitude20 {
     
-    public static String getGreeetingRecipient() {
+    public static String getGreetingRecipient() {
         Scanner input = new Scanner(System.in);
         System.out.print("Input the name of people you want to greet: ");
         String recipientName = input.nextLine();
@@ -11,12 +11,18 @@ public class ExpressingGratitude20 {
     }
 
     public static void sayThankyou() {
-        String name = getGreeetingRecipient();
-        System.out.println("Thank you " + name + " for being the best teacher in the world. \n" +
-        "You inspired me a love for learning and made me feel like I could ask you anything.");
+        String name = getGreetingRecipient();
+        System.out.println("Thank you " + name + " for being the best teacher in the world.\n" +
+        "You inspired in me a love for learning and made me feel like I could ask you anything.");
+    }
+
+    public static void sayAdditionalGreetings(String remark) {
+        System.out.println(remark);
     }
 
     public static void main(String[] args) {
         sayThankyou();
+        String expression = "I hope you continue to inspire many students in the future!";
+        sayAdditionalGreetings(expression);
     }
 }
